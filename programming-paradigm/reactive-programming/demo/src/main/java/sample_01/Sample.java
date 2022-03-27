118e2313f3f362a042f6b0ab89f72006bdc3fde8
evan.h <id920809@naver.com>
2022-03-28 00:57:19 +0900

    RxJava Sample 코드 작성

---
+++ programming-paradigm/reactive-programming/demo/src/main/java/sample_01/Sample.java
@@ -0,0 +1,25 @@
+package sample_01;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
+public class Sample {
+    public static void main(String[] args) throws InterruptedException {
+        Observable.just(100, 200, 300, 400, 500)
+                .doOnNext(data -> System.out.println(getThreadName() + " : #doOnNext() : " + data))
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.computation())
+                .filter(num -> {
+                    System.out.println(getThreadName() + " : #filter() : " + num);
+                    return num > 300;
+                })
+                .subscribe(num -> System.out.println(getThreadName() + " : result : " + num))
+        ;
+
+        Thread.sleep(500);
+    }
+
+    public static String getThreadName() {
+        return Thread.currentThread().getName();
+    }
+}
